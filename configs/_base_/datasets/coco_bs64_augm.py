2def549223b0d2ba5dd2c35ec081dd55ccd8de11
cancam <bariscancam@gmail.com>
2020-11-02 16:19:49 +0300

    img dtype corrected for photometricdistortion step of preprocessing.

--- configs/_base_/datasets/coco_bs64_augm.py
+++ configs/_base_/datasets/coco_bs64_augm.py
@@ -3,7 +3,7 @@ dataset_type = 'ImageNet'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', to_float32=True),
     # load annotations yok çünkü folderlarda annotasyonlar var.
     dict(
         type='PhotoMetricDistortion',
